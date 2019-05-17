cd8e5729c29e0eb33dfa95d2c580d059d9279307
mattapattu <mattapattu@gmail.com>
2019-05-17 18:50:09 +0200

    latest

--- Dnf_1D_fft_Neural_robot.py
+++ Dnf_1D_fft_Neural_robot.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/en
 # -*- coding: utf-8 -*-
 
 import sys
+import os,time
+import pexpect
+import re
 import numpy as np
 import matplotlib.pyplot as plt
 #matplotlib.use('Agg')
@@ -10,11 +13,11 @@ from inputDNF_1D import *
 from pylab import *
 import operator
 from collections import defaultdict
-from time import sleep
 #import processImage.PIXY_MLP_enregistrement as imgprocess
 #import pixy.build.libpixyusb_swig.pixy as pixy1
 from mlp import *
 from robot.PyCherokeyRobot.pyCherokeyRobot.pc2Robot.ChRobot import *
+import matplotlib.animation as animation
 
 
 
@@ -44,7 +47,45 @@ global totalInput
 peakOfActivation=0.0
 xmax = 0
 
+def connectToSpiky():
+    global spinnTrue
+    if spinnTrue is True:
+        print "Connecting to spiky"
+        child = pexpect.spawn('ssh spiky@134.59.130.214')
+        child1 = pexpect.spawn('ssh spiky@134.59.130.214')
+        child.logfile = open("/tmp/mylog", "w")
+        child1.logfile = open("/tmp/mylog1", "w")
+        child.sendline('ls')
+        child1.sendline('ls')
+        print "Connected"
+    else:
+        child = object()
+        child1 = object()
+    return child,child1
 
+def setupSpiky(child,child1):
+    #child = pexpect.spawn('ssh spiky@134.59.130.214')
+    print "Inside setup spiky"
+    child.expect('spiky\@spiky-server:\~\$')
+    child.sendline('spinn4_PyNN08')
+    child.expect('\(spinn4_PyNN08\).*spiky\@spiky-server:\~\$')
+    child.sendline('cd ajames')
+
+
+    #child1 = pexpect.spawn('ssh spiky@134.59.130.214')
+    child1.expect('spiky\@spiky-server:\~\$')
+    child1.sendline('spinn4_PyNN08')
+    child1.expect('\(spinn4_PyNN08\).*spiky\@spiky-server:\~\$')
+    child1.sendline('cd ajames')
+
+    child.sendline('python sender.py')
+    child.expect('WARNING:')
+    time.sleep(2)
+    child1.sendline('python DNF2.py')
+    time.sleep(2)
+    return child, child1
+
+  
 def motorResponse(inputDNFActivation):
      # Creation layer 1 (300 neurons, each with 785 inputs) ==> HIDDEN LAYER
     #
@@ -65,7 +106,110 @@ def motorResponse(inputDNFActivation):
     #neural_network.print_output()
     return neural_network.return_output()
 
+def motorResponseFromSpinnaker(inputDNFActivation,child,child1):
+
+    print "Inside motorReponse From Spinnaker"    
+    leftSpeed = 0.5
+    rightSpeed = 0.5
+    #print inputDNFActivation
+    inputpixels = np.where(inputDNFActivation >= 0.01) 
     
+    if inputpixels[0].size:
+        #str1 = " ".join(inputpixels[0]) 
+        str1 = ' '.join(map(str, inputpixels[0]))
+        print (str1)
+        #print "Sending input to spiky"
+        #child.sendcontrol('j')
+        try :
+            child.expect('Input neurons to spike')
+        except pexpect.exceptions.TIMEOUT:
+            child.send('\r\n')
+            child.expect('Input neurons to spike')
+
+        child.sendline(str1)
+        child.sendcontrol('j')
+        child1.sendcontrol('j')
+        child1.expect('Received spikes from population')
+        #print child1.after
+        try:
+            print "Looking for neuron rates"
+            child1.expect(r'neuron0 rate =  \d+\.\d+',timeout=1)
+            line = child1.after
+            print ('line is: ',line)
+
+            #pattern = re.compile("neuron0 rate =  (\d+\.\d+).*neuron1 rate =  (\d+\.\d+).*neuron2 rate =  (\d+\.\d+).*neuron3 rate =  (\d+\.\d+).*neuron4 rate =  (\d+\.\d+).*neuron5 rate =  (\d+\.\d+)")
+            x1 = re.match("neuron0 rate =  (\d+\.\d+)",line)
+
+            child1.expect(r'neuron1 rate =  \d+\.\d+',timeout=1)
+            line = child1.after
+            print ('line is: ',line)
+            x2 = re.match("neuron1 rate =  (\d+\.\d+)",line)
+    
+            child1.expect(r'neuron2 rate =  \d+\.\d+',timeout=1)
+            line = child1.after
+            print ('line is: ',line)
+            x3 = re.match("neuron2 rate =  (\d+\.\d+)",line)
+
+            child1.expect(r'neuron3 rate =  \d+\.\d+',timeout=1)
+            line = child1.after
+            print ('line is: ',line)
+            x4 = re.match("neuron3 rate =  (\d+\.\d+)",line)
+
+            child1.expect(r'neuron4 rate =  \d+\.\d+',timeout=1)
+            line = child1.after
+            print ('line is: ',line)
+            x5 = re.match("neuron4 rate =  (\d+\.\d+)",line)
+
+            child1.expect(r'neuron5 rate =  \d+\.\d+',timeout=1)
+            line = child1.after
+            print ('line is: ',line)
+            x6 = re.match("neuron5 rate =  (\d+\.\d+)",line)
+
+                   
+            if x1 and x2 and x3 and x4 and x5 and x6:
+                print("Pattern matched")
+                x1rate = x1.group(1)
+                x2rate = x2.group(1)
+                x3rate = x3.group(1)
+                x4rate = x4.group(1)
+                x5rate = x4.group(1)
+                x6rate = x5.group(1)
+
+                print(x1rate,x2rate,x3rate,x4rate,x5rate,x6rate)
+                maxval = max(x1rate,x2rate,x3rate,x4rate,x5rate,x6rate)
+                print "maxval is ",maxval
+
+                if x1rate == maxval :
+                    leftSpeed = 0.45
+                    rightSpeed = 0.30
+                elif x2rate == maxval  : 
+                    leftSpeed = 0.41
+                    rightSpeed = 0.32
+                elif x3rate == maxval :
+                    leftSpeed = 0.35
+                    rightSpeed = 0.35
+                elif x4rate == maxval :
+                    leftSpeed = 0.35
+                    rightSpeed = 0.35
+                elif x5rate == maxval :
+                    leftSpeed = 0.41
+                    rightSpeed = 0.32
+                elif x6rate == maxval :
+                    leftSpeed = 0.41
+                    rightSpeed = 0.32
+                else:
+                    leftSpeed = 0.5
+                    rightSpeed = 0.5
+            else:
+                print("No pattern matched")
+        except pexpect.exceptions.TIMEOUT:
+            print('No neurons fired')
+            leftSpeed = 0.5
+            rightSpeed = 0.5
+            #   else:
+    print("Returning speeds:",leftSpeed,rightSpeed)
+
+    return leftSpeed,rightSpeed   
 
 def f(x, threshold_f):
     ''' sigmoidal function
@@ -83,6 +227,7 @@ def f(x, threshold_f):
     global xmax
     
     xmax1,peak1 = max(enumerate(fval), key=operator.itemgetter(1))
+
     if(peak1 >= peakOfActivation):
         xmax, peakOfActivation = xmax1,peak1
         xmax = xmax-159.5
@@ -154,7 +299,20 @@ if __name__ == '__main__':
     robot = ChRobot(HOST="192.168.137.254")
     robot.setRightSpeed(0.5)
     robot.setLeftSpeed(0.5)
-    
+   
+    ### Connect to spiky
+    global spinnTrue
+    if "spinnaker" in sys.argv[1]:
+        spinnTrue = True
+    else :
+        spinnTrue = False
+
+    child,child1 = connectToSpiky()
+
+    if "spinnaker" in sys.argv[1]:
+        setupSpiky(child,child1) 
+     
+
     #Rest threshold, activation threshold and initialization of the population's potential
     H = np.ones(len(X)) * h
     thres = np.ones(len(X),dtype=np.float128) * threshold
@@ -197,34 +355,34 @@ if __name__ == '__main__':
            u += du
            updatedActivation = f(u,threshold)
            #print("updated activation u = ", updatedActivation)
-           #print("Waiting for motor response") 
-           resp = motorResponse(updatedActivation)
-           print(resp[0],resp[1])
-           speedDiff = abs(resp[0]-resp[1]) 
-           turnLeft = False
-           if resp[0] < resp [1]:
-                   turnLeft = True 
-           print("Diff between left & right speeds is :",speedDiff)
-           print("turnLeft is :", turnLeft)
-
-           if speedDiff < 100 and turnLeft:
-               leftSpeed = 0.35
-               rightSpeed = 0.40
-           elif speedDiff < 100 and not turnLeft:
-               leftSpeed = 0.40
-               rightSpeed =  0.35
-           elif speedDiff > 100 and speedDiff < 9000 and turnLeft:
-               leftSpeed = 0.40
-               rightSpeed = 0.30
-           elif speedDiff > 100 and speedDiff < 9000 and not turnLeft:
-               leftSpeed = 0.30
-               rightSpeed = 0.40
-           elif speedDiff >= 9000 and turnLeft:
-               leftSpeed = 0.45
-               rightSpeed = 0.30
-           elif speedDiff >= 9000 and not turnLeft:
-               leftSpeed = 0.30
-               rightSpeed = 0.45
+           '''
+           if spinnTrue is True:
+               leftSpeed,rightSpeed = motorResponseFromSpinnaker(updatedActivation,child,child1)
+           else:
+               resp = motorResponse(updatedActivation)
+               speedDiff = abs(resp[0]-resp[1])
+               turnLeft = False
+               if resp[0] < resp [1]:
+                turnLeft = True
+               
+               if speedDiff < 100 and turnLeft:
+                   leftSpeed = 0.35
+                   rightSpeed = 0.35
+               elif speedDiff < 100 and not turnLeft:
+                   leftSpeed = 0.35
+                   rightSpeed =  0.35
+               elif speedDiff > 100 and speedDiff < 9000 and turnLeft:
+                   leftSpeed = 0.41
+                   rightSpeed = 0.32
+               elif speedDiff > 100 and speedDiff < 9000 and not turnLeft:
+                   leftSpeed = 0.32
+                   rightSpeed = 0.41
+               elif speedDiff >= 9000 and turnLeft:
+                   leftSpeed = 0.45
+                   rightSpeed = 0.30
+               elif speedDiff >= 9000 and not turnLeft:
+                   leftSpeed = 0.30
+                   rightSpeed = 0.45
            #print(np.unique(updatedActivation).size)
            if np.unique(updatedActivation).size == 1:
                robot.setRightSpeed(0.5)
@@ -235,6 +393,7 @@ if __name__ == '__main__':
                robot.setRightSpeed(leftSpeed)
                robot.setLeftSpeed(rightSpeed)
            #print("Received motor response") 
+           '''
            sig_act.set_ydata(updatedActivation)
            act.set_ydata(u)
            plt.draw()
@@ -242,6 +401,41 @@ if __name__ == '__main__':
        #sig_act.set_ydata(updatedActivation)
        #act.set_ydata(u)
        #plt.draw()
+       if spinnTrue is True:
+           leftSpeed,rightSpeed = motorResponseFromSpinnaker(updatedActivation,child,child1)
+       else:
+           resp = motorResponse(updatedActivation)
+           speedDiff = abs(resp[0]-resp[1])
+           turnLeft = False
+           if resp[0] < resp [1]:
+               turnLeft = True
+           if speedDiff < 100 and turnLeft:
+               leftSpeed = 0.35
+               rightSpeed = 0.35
+           elif speedDiff < 100 and not turnLeft:
+               leftSpeed = 0.35
+               rightSpeed =  0.35
+           elif speedDiff > 100 and speedDiff < 9000 and turnLeft:
+               leftSpeed = 0.41
+               rightSpeed = 0.32
+           elif speedDiff > 100 and speedDiff < 9000 and not turnLeft:
+               leftSpeed = 0.32
+               rightSpeed = 0.41
+           elif speedDiff >= 9000 and turnLeft:
+               leftSpeed = 0.45
+               rightSpeed = 0.30
+           elif speedDiff >= 9000 and not turnLeft:
+               leftSpeed = 0.30
+               rightSpeed = 0.45
+           if np.unique(updatedActivation).size == 1:
+              robot.setRightSpeed(0.5)
+              robot.setLeftSpeed(0.5)
+              print(0.5,0.5)
+           else:
+              print(leftSpeed,rightSpeed)
+       robot.setRightSpeed(leftSpeed)
+       robot.setLeftSpeed(rightSpeed)
+
        plt.show()
        
        pause(0.1)
@@ -267,25 +461,12 @@ if __name__ == '__main__':
                count = count+1
                print('[BLOCK_TYPE=%d SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' % (block[0], block[1], block[2], block[3], block[4], block[5]))
                xpos = block[2]-159.5
-       #        try:
-       #            print("Capturing frame")
-                   #frame = imgprocess.acquisition(block[2],block[3])
-                   
-                   #print(frame)
-                   #cv2.imshow('Test image',frame)
-                   #cv2.waitKey()
-
-       #        except ValueError as e:
-        #           print("Exception thrown - ",e.args)
-                   
-               ## Send frame to CNN
-               ## Get response from CNN and if true, add input to totalinput
-               
-               #print("add to total input")
-               totalInput = totalInput + (block[4]*block[5]/600)* gaussian(X,mu=xpos,sigma=sigma)
+               totalInput = totalInput + (block[4]*block[5]/110)* gaussian(X,mu=xpos,sigma=sigma)
            updateDNF(totalInput,u) 
        else :
            totalInput = np.zeros(320,)
         #   print("Updating DNF with empty input")
            updateDNF(totalInput,u)     
-        
+
+ 
+
